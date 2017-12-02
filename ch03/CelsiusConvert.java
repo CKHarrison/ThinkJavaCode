04dc09c0c13fa3330317ca897d8fab83a7a58c44
CKHarrison <hirohumi1882@gmail.com>
2017-12-01 17:47:50 -0800

    solutions for exercise 3.1 and 3.2

---
+++ ch03/CelsiusConvert.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+//Converting Celsius to Fahrenheit
+
+public class CelsiusConvert {
+
+    public static void main(String[] args) {
+        double celsiusTemp;
+        int feet, inches, remainder;
+        
+        final int IN_PER_FOOT = 12;
+        Scanner in = new Scanner(System.in);
+
+        // prompt the user and get the value
+        System.out.print("Please input temperature in Celsius: ");
+        celsiusTemp = in.nextDouble();
+        
+        //Convert and output the result
+        double fahrenheit = (celsiusTemp *(9.0/5.0)) + 32;
+        System.out.printf("%.2f Celsius = %.2f Fahrenheit \n",
+                          celsiusTemp, fahrenheit);
+    }
+
+}
---
+++ ch03/Test.java
@@ -0,0 +1,10 @@
+public class Test {
+
+    public static void main(String[] args) {
+        int cm = 1;
+        double feet = 2.0;
+        System.out.printf("%.2f cm = %d in\n",
+                          feet);
+    }
+
+}
