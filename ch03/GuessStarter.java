1a0454f60a23207125a0d038d4cc339bf1b97d5e
CKHarrison <hirohumi1882@gmail.com>
2017-12-01 18:46:59 -0800

    Solution for all problems now added

--- ch03/GuessStarter.java
+++ ch03/GuessStarter.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+import java.util.Scanner;
 
 /**
  * Starter code for the "Guess My Number" exercise.
@@ -6,10 +7,20 @@ import java.util.Random;
 public class GuessStarter {
 
     public static void main(String[] args) {
+        int userNumber;
+        int difference;
+        
         // pick a random number
         Random random = new Random();
         int number = random.nextInt(100) + 1;
-        System.out.println(number);
+        //Ask user for number
+        System.out.println("Please Enter a number between 0 and 100 ");
+        Scanner in = new Scanner(System.in);
+        userNumber = in.nextInt();
+        difference = number - userNumber;
+        System.out.println("The difference between the computer's number and your number is " + difference);
+        System.out.println("Computer's number: " + number + ", Your number: " + userNumber);
+        //System.out.println(number);
     }
 
 }
